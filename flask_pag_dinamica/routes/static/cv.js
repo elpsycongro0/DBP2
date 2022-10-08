19bdcf3fb1bb5107b304a648798890310ae589e2
elpsycongro0 <elpsycongro0@github.com>
2022-10-07 23:45:30 -0500

    pagina dinamica completa

--- flask_pag_dinamica/routes/static/cv.js
+++ flask_pag_dinamica/routes/static/cv.js
@@ -1,26 +1,225 @@
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#submit').onclick = () => {
-        const experienceInstance = document.createElement("div");
-        experienceInstance.classList.add("medsmall");
-
-        const cargo = document.createElement('b');
-        cargo.innerHTML = document.querySelector('#cargo').value;
-        experienceInstance.appendChild(cargo);
-        experienceInstance.appendChild(document.createElement('br'));
-
-        const lugar = document.createElement('b');
-        const lugartitulo = document.createElement('i');
-        lugartitulo.innerHTML = document.querySelector('#lugar').value;
-        lugar.appendChild(lugartitulo);
-        experienceInstance.appendChild(lugar);
-        experienceInstance.innerHTML += " | "+document.querySelector('#periodo').value;
-
-        document.querySelector('#experiencia').appendChild(experienceInstance);
-
-        document.querySelector('#lugar').value = ""
-        document.querySelector('#cargo').value = ""
-        document.querySelector('#periodo').value = ""
-
-    };
     
+    document.querySelector('#mas-experiencia').onclick = () => {
+
+        if(document.querySelector('#add-experience')!=null){
+            return;
+        }
+        const myDiv = document.createElement("div");
+
+        const experienceForm = document.createElement('form');
+        experienceForm.setAttribute('id','add-experience');
+        experienceForm.setAttribute('method','POST');
+        myDiv.appendChild(experienceForm);
+
+        const input1 = document.createElement('input');
+        input1.setAttribute('id','cargo');
+        input1.setAttribute('autocomplete','off');
+        input1.setAttribute('placeholder','Cargo');
+        input1.setAttribute('type','text');
+        experienceForm.appendChild(input1);
+
+        const input2 = document.createElement('input');
+        input2.setAttribute('id','lugar');
+        input2.setAttribute('autocomplete','off');
+        input2.setAttribute('placeholder','Lugar');
+        input2.setAttribute('type','text');
+        experienceForm.appendChild(input2);
+
+        const input3 = document.createElement('input');
+        input3.setAttribute('id','periodo');
+        input3.setAttribute('autocomplete','off');
+        input3.setAttribute('placeholder','Periodo');
+        input3.setAttribute('type','text');
+        experienceForm.appendChild(input3);
+
+        const submitButton = document.createElement('button');
+        submitButton.setAttribute('id','submit');
+        submitButton.innerHTML +='Enviar';
+        myDiv.appendChild(submitButton);
+        
+        document.querySelector('#experiencia').appendChild(myDiv);
+        
+        submitButton.onclick = () => {
+            const experienceInstance = document.createElement("div");
+            experienceInstance.classList.add("medsmall");
+    
+            const cargo = document.createElement('b');
+            cargo.innerHTML = document.querySelector('#cargo').value;
+            experienceInstance.appendChild(cargo);
+            experienceInstance.appendChild(document.createElement('br'));
+    
+            const lugar = document.createElement('b');
+            const lugartitulo = document.createElement('i');
+            lugartitulo.innerHTML = document.querySelector('#lugar').value;
+            lugar.appendChild(lugartitulo);
+            experienceInstance.appendChild(lugar);
+            experienceInstance.innerHTML += " | "+document.querySelector('#periodo').value;
+    
+            document.querySelector('#experiencia').appendChild(experienceInstance);
+    
+            myDiv.remove();
+        };
+    };
+    document.querySelector('#mas-formacion').onclick = () => {
+
+        if(document.querySelector('#add-formacion')!=null){
+            return;
+        }
+        const myDiv = document.createElement("div");
+
+        const experienceForm = document.createElement('form');
+        experienceForm.setAttribute('id','add-formacion');
+        experienceForm.setAttribute('method','POST');
+        myDiv.appendChild(experienceForm);
+
+        const input1 = document.createElement('input');
+        input1.setAttribute('id','grado');
+        input1.setAttribute('autocomplete','off');
+        input1.setAttribute('placeholder','Grado');
+        input1.setAttribute('type','text');
+        experienceForm.appendChild(input1);
+
+        const input2 = document.createElement('input');
+        input2.setAttribute('id','lugar');
+        input2.setAttribute('autocomplete','off');
+        input2.setAttribute('placeholder','Lugar');
+        input2.setAttribute('type','text');
+        experienceForm.appendChild(input2);
+
+        const input3 = document.createElement('input');
+        input3.setAttribute('id','periodo');
+        input3.setAttribute('autocomplete','off');
+        input3.setAttribute('placeholder','Periodo');
+        input3.setAttribute('type','text');
+        experienceForm.appendChild(input3);
+
+        const submitButton = document.createElement('button');
+        submitButton.setAttribute('id','submit');
+        submitButton.innerHTML +='Enviar';
+        myDiv.appendChild(submitButton);
+        
+        document.querySelector('#formacion').appendChild(myDiv);
+        
+        submitButton.onclick = () => {
+            const experienceInstance = document.createElement("div");
+            experienceInstance.classList.add("medsmall");
+    
+            const cargo = document.createElement('b');
+            cargo.innerHTML = document.querySelector('#grado').value;
+            experienceInstance.appendChild(cargo);
+            experienceInstance.appendChild(document.createElement('br'));
+    
+            const lugar = document.createElement('b');
+            const lugartitulo = document.createElement('i');
+            lugartitulo.innerHTML = document.querySelector('#lugar').value;
+            lugar.appendChild(lugartitulo);
+            experienceInstance.appendChild(lugar);
+            experienceInstance.innerHTML += " | "+document.querySelector('#periodo').value;
+    
+            document.querySelector('#formacion').appendChild(experienceInstance);
+    
+            myDiv.remove();
+        };
+    };
+    document.querySelector('#mas-idiomas').onclick = () => {
+        if(document.querySelector('#add-idioma')!=null){
+            return;
+        }
+        const myDiv = document.createElement("div");
+
+        const experienceForm = document.createElement('form');
+        experienceForm.setAttribute('id','add-idioma');
+        experienceForm.setAttribute('method','POST');
+        myDiv.appendChild(experienceForm);
+
+        const input1 = document.createElement('input');
+        input1.setAttribute('id','idioma');
+        input1.setAttribute('autocomplete','off');
+        input1.setAttribute('placeholder','Idioma');
+        input1.setAttribute('type','text');
+        experienceForm.appendChild(input1);
+
+        const input2 = document.createElement('input');
+        input2.setAttribute('id','nivel');
+        input2.setAttribute('autocomplete','off');
+        input2.setAttribute('placeholder','Nivel');
+        input2.setAttribute('type','text');
+        experienceForm.appendChild(input2);
+
+        const submitButton = document.createElement('button');
+        submitButton.innerHTML +='Enviar';
+        myDiv.appendChild(submitButton);
+        
+        document.querySelector('#idiomas').appendChild(myDiv);
+        
+        submitButton.onclick = () => {
+            const maindiv = document.createElement("div");
+            maindiv.innerHTML+=document.querySelector('#idioma').value+": "+document.querySelector('#nivel').value+"<br>";
+            document.querySelector('#idiomas').appendChild(maindiv);
+            myDiv.remove();
+        };
+    };
+    document.querySelector('#mas-aptitudes').onclick = () => {
+        if(document.querySelector('#add-aptitudes')!=null){
+            return;
+        }
+        const myDiv = document.createElement("div");
+
+        const experienceForm = document.createElement('form');
+        experienceForm.setAttribute('id','add-aptitudes');
+        experienceForm.setAttribute('method','POST');
+        myDiv.appendChild(experienceForm);
+
+        const input1 = document.createElement('input');
+        input1.setAttribute('id','nuevaAptitud');
+        input1.setAttribute('autocomplete','off');
+        input1.setAttribute('placeholder','Nueva aptitud');
+        input1.setAttribute('type','text');
+        experienceForm.appendChild(input1);
+
+        const submitButton = document.createElement('button');
+        submitButton.innerHTML +='Enviar';
+        myDiv.appendChild(submitButton);
+        
+        document.querySelector('#aptitudes').appendChild(myDiv);
+        
+        submitButton.onclick = () => {
+            const maindiv = document.createElement("div");
+            maindiv.innerHTML+="·  "+document.querySelector('#nuevaAptitud').value;
+            document.querySelector('#aptitudes').appendChild(maindiv);
+            myDiv.remove();
+        };
+    };
+    document.querySelector('#mas-habilidades').onclick = () => {
+        if(document.querySelector('#add-habilidades')!=null){
+            return;
+        }
+        const myDiv = document.createElement("div");
+
+        const experienceForm = document.createElement('form');
+        experienceForm.setAttribute('id','add-habilidades');
+        experienceForm.setAttribute('method','POST');
+        myDiv.appendChild(experienceForm);
+
+        const input1 = document.createElement('input');
+        input1.setAttribute('id','nuevaHabilidad');
+        input1.setAttribute('autocomplete','off');
+        input1.setAttribute('placeholder','Nueva habilidad');
+        input1.setAttribute('type','text');
+        experienceForm.appendChild(input1);
+
+        const submitButton = document.createElement('button');
+        submitButton.innerHTML +='Enviar';
+        myDiv.appendChild(submitButton);
+        
+        document.querySelector('#habilidades').appendChild(myDiv);
+        
+        submitButton.onclick = () => {
+            const maindiv = document.createElement("div");
+            maindiv.innerHTML+="·  "+document.querySelector('#nuevaHabilidad').value;
+            document.querySelector('#habilidades').appendChild(maindiv);
+            myDiv.remove();
+        };
+    };
 });
--- flask_pag_dinamica/routes/static/validation.js
+++ flask_pag_dinamica/routes/static/validation.js
@@ -1,20 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#submit').disabled = true;
+    
+    mainForm = document.forms[0];
+    mainFormNames = []
+    // fill all names in form
     document.querySelectorAll('.checked_empty').forEach(input => {
-        input.onkeyup = () => {
-            allIputs = document.querySelectorAll('.checked_empty');
+        mainFormNames.push(input.name);
+    });
+    // input validation event listeners
+    document.querySelectorAll('.checked_empty').forEach(input => {
+        input.onchange = () => {
             disabledSubmit = false;
-            for (var i = 0; i < allIputs.length; i++) {
-                if(allIputs[i].value.length == 0){
+            for(var i=0; i<mainFormNames.length; i++){
+                console.log(mainFormNames[i],":", mainForm[mainFormNames[i]].value);
+                if(mainForm[mainFormNames[i]].value.length == 0){
                     disabledSubmit = true;
                 }
             }
-            document.querySelector('#submit').disabled = disabledSubmit;    
-            
-        };
+            document.querySelector('#submit').disabled = disabledSubmit;
+        }
+    });
+    document.querySelectorAll('.text_input').forEach(input => {
         input.onblur = () => {
             input.value = input.value.replace(/\s/g, '')
         }
     });
-    
 });
